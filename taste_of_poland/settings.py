c18a062871277f7fe409be10e25c9359b009992b
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-01-04 00:36:57 +0000

    Deploy to Heroku

--- taste_of_poland/settings.py
+++ taste_of_poland/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['taste_of_poland.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['taste-of-poland.herokuapp.com', 'localhost']
 
 
 # Application definition
