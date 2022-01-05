9ccb7b33009fa2fc8545a69c12b8264d49b5a227
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-01-05 23:28:21 +0000

    Create Reservation model

--- table_reservation/models.py
+++ table_reservation/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_user")
+    first_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_fname")
+    last_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_lname")
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_email")
+    # status 
+    time_and_date = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    party_size = models.IntegerField(default=1)
+    additional_info = models.TextField(blank=True, null=True)
+    name = models.TextField()
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
