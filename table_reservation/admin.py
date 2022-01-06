f98dc57311d896add8e6ae5527d4d6edc01e7424
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-01-06 23:58:33 +0000

    Modify Reservation model

--- table_reservation/admin.py
+++ table_reservation/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Reservation
+from .models import Reservation, Table
 
 
+admin.site.register(Table)
 admin.site.register(Reservation)
+
--- table_reservation/models.py
+++ table_reservation/models.py
@@ -3,17 +3,46 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
+# class Customer(models.Model):
+#     user = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="booking_user",
+#         default=None)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     email = models.EmailField()
+
+
+class Table(models.Model):
+    # seats = models.IntegerField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+
 class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_user", default=None)
-    first_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_fname", default=None)
-    last_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_lname", default=None)
-    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_email", default=None)
-    # status 
-    time_and_date = models.DateTimeField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    table = models.ForeignKey('Table', on_delete=models.CASCADE, default=None)
+    booker = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     party_size = models.IntegerField(default=1)
+    time_and_date = models.DateTimeField(default=None)
     additional_info = models.TextField(blank=True, null=True)
-    
-    def __str__(self):
-        return '%s %s' % (self.first_name, self.last_name)
+
+
+# class Reservation(models.Model):
+#     user = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name="booking_user",
+#         default=None)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     email = models.EmailField()
+#     # status
+#     time_and_date = models.DateTimeField()
+#     created_on = models.DateTimeField(auto_now_add=True)
+#     updated_on = models.DateTimeField(auto_now=True)
+#     party_size = models.IntegerField(default=1)
+#     additional_info = models.TextField(blank=True, null=True)
+#     approved = models.BooleanField(default=False)
+
+# def __str__(self):
+#     return self.title
+
+#     def __str__(self):
+#         return '%s %s' % (self.first_name, self.last_name)
