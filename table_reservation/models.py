0a73d6a0b5ef7660deb627f1348252a92c341d53
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-01-06 18:16:07 +0000

    Migrate changes to database

--- table_reservation/models.py
+++ table_reservation/models.py
@@ -4,10 +4,10 @@ from cloudinary.models import CloudinaryField
 
 
 class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_user")
-    first_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_fname")
-    last_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_lname")
-    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_email")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_user", default=None)
+    first_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_fname", default=None)
+    last_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_lname", default=None)
+    email = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_email", default=None)
     # status 
     time_and_date = models.DateTimeField()
     created_on = models.DateTimeField(auto_now_add=True)
