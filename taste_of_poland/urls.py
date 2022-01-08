57f9f2250f144b19e5785cbc452a4e87aafbb2eb
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-01-08 22:17:23 +0000

    Add templates folder

--- taste_of_poland/urls.py
+++ taste_of_poland/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from table_reservation.views import say_hello
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', say_hello, name='home')
 ]
